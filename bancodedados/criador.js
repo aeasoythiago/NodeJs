b293db844eb07b5e0deadf62fb58a06d42b55196
aeasoythiago <thiagomecena@hotmail.com>
2025-05-14 23:31:27 -0300

    Removi comentário

--- bancodedados/criador.js
+++ bancodedados/criador.js
@@ -5,10 +5,6 @@ const novoUsuario = new mongoose.Schema({
         type: String,
         required: true,
     },
-    //segundonome: {
-      //  type: String,
-        //required: true,
-    //},
     email: {
         type: String,
         required: true,
